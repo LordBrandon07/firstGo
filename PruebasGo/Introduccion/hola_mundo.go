068d9b48e05d40ead51e318d15324e1bd517d7e9
LordBrandon07 <brandonnrodriguez07@gmail.com>
2024-03-27 22:33:42 -0500

    helloworld

---
+++ PruebasGo/Introduccion/hola_mundo.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("viva colombia, viva falcao")
+}
---
+++ PruebasGo/Introduccion/nombre.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const name = "Pepe"
+	fmt.Println("my name is", name)
+}
